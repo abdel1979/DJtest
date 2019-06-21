da9874b59f646b0b78dfdb173ca5497363615078
abdel1979 <47523854+abdel1979@users.noreply.github.com>
2019-06-21 10:54:19 +0100

    Update views.py

--- newapp/views.py
+++ newapp/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 def index(request):
-    return HttpResponse(" Django on Docker on AWS ")
+    return HttpResponse(" Salam World - this is our first Django on Docker accesible from AWS ")
